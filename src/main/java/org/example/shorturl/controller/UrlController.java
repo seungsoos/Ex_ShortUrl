1020d6f94d5ad6f23ba8fd6cd40b2477f3766e54
seungsoo <seung_soos@naver.com>
2024-03-30 19:18:08 +0900

    - feat : redirect Controller 추가

--- src/main/java/org/example/shorturl/controller/UrlController.java
+++ src/main/java/org/example/shorturl/controller/UrlController.java
@@ -1,11 +1,15 @@
 package org.example.shorturl.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.shorturl.domain.dto.request.CreateShortUrlRequest;
+import org.example.shorturl.domain.dto.response.GetAllUrlsResponse;
+import org.example.shorturl.domain.enums.UrlType;
 import org.example.shorturl.service.UrlService;
-import org.example.shorturl.service.UrlServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,8 +18,35 @@ public class UrlController {
 
     private final UrlService urlService;
 
+    /**
+     * URL 전체조회
+     * TYPE - ALL / ORIGIN / SHORT
+     */
     @GetMapping("/all")
-    public void getAllUrls() {
-        urlService.getAllUrls();
+    public List<GetAllUrlsResponse> getAllUrls(@RequestParam UrlType urlType) {
+        List<GetAllUrlsResponse> allUrls = urlService.getAllUrls(urlType);
+        return allUrls;
     }
+
+    /**
+     * URL 상세조회
+     */
+    @GetMapping("/detail/{id}")
+    public void getDetailUrl(@PathVariable Long id) {
+
+    }
+
+    /**
+     * URL 변환
+     * originalUrl -> shortUrl
+     */
+    @PostMapping
+    public ResponseEntity<String> createShortUrl(@RequestBody @Valid CreateShortUrlRequest createShortUrlRequest) {
+        String shortUrl = urlService.createShortUrl(createShortUrlRequest);
+        return ResponseEntity.ok().body(shortUrl);
+    }
+
+
+
+
 }
